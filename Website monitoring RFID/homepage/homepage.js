a8d01e60bc869b55b539429b528696f8717a059f
anmegan <154001697+anmegan@users.noreply.github.com>
2025-03-11 13:08:24 +0700

    Add files via upload

---
+++ Website monitoring RFID/homepage/homepage.js
@@ -0,0 +1,64 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB6c0FpgDGGSjLqJWQQgk7NMJdcrJXPy_8",
+  authDomain: "login-register-d71d9.firebaseapp.com",
+  databaseURL: "https://login-register-d71d9-default-rtdb.firebaseio.com",
+  projectId: "login-register-d71d9",
+  storageBucket: "login-register-d71d9.firebasestorage.app",
+  messagingSenderId: "520309435822",
+  appId: "1:520309435822:web:fbdfc23756822a31e9ec3f",
+  measurementId: "G-Z3H8RF1QHK"
+};
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const auth=getAuth();
+  const db=getFirestore();
+
+  onAuthStateChanged(auth, (user)=>{
+    const loggedInUserId=localStorage.getItem('loggedInUserId');
+    if(loggedInUserId){
+        console.log(user);
+        const docRef = doc(db, "users", loggedInUserId);
+        getDoc(docRef)
+        .then((docSnap)=>{
+            if(docSnap.exists()){
+                const userData=docSnap.data();
+                document.getElementById('loggedUserFName').innerText=userData.firstName;
+                document.getElementById('loggedUserEmail').innerText=userData.email;
+                document.getElementById('loggedUserLName').innerText=userData.lastName;
+
+            }
+            else{
+                console.log("no document found matching id")
+            }
+        })
+        .catch((error)=>{
+            console.log("Error getting document");
+        })
+    }
+    else{
+        console.log("User Id not Found in Local storage")
+    }
+  })
+
+  const logoutButton=document.getElementById('logout');
+
+  logoutButton.addEventListener('click',()=>{
+    localStorage.removeItem('loggedInUserId');
+    signOut(auth)
+    .then(()=>{
+        window.location.href='index.html';
+    })
+    .catch((error)=>{
+        console.error('Error Signing out:', error);
+    })
+  })
+
